dde86246e0a335d1186a63cf34ef77e98167882c
Idan Moral <idan22moral@gmail.com>
2021-02-12 15:40:45 +0200

    Made the app's files served instead of being local

--- client/main.js
+++ client/main.js
@@ -1,9 +1,9 @@
 const {app, Dock, BrowserWindow, CommandLine} = require('electron');
-const io = require('socket.io-client');
 
 app.whenReady().then(() => {
     let win = new BrowserWindow({ width: 1920, height: 1080, show: false });
     win.loadFile('public/index.html')
+    win.loadURL('https://video.hexionteam.com')
     win.once('ready-to-show', () => {
         win.show();
     });
