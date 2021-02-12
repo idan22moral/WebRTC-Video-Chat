0e35af09208a16ecada72a3f60d03d5196670a9c
Idan Moral <idan22moral@gmail.com>
2021-02-12 17:08:11 +0200

    Added logger to files server

--- server/index.js
+++ server/index.js
@@ -2,5 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const morgan = require('morgan');
+app.use(morgan('common'));
+
 app.use(express.static('public'));
 app.listen(PORT, () => console.log(`Serving files on port: ${PORT}`));
